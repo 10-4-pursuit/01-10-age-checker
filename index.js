9e2ae16f381ada2725b30fab4649039adf1c6e97
lanell1 <lanelllopez@pursuit.org>
2023-12-09 15:06:34 -0500

    complete-01-10-age-checker

--- index.js
+++ index.js
@@ -3,7 +3,26 @@ const ages = [5, 15, 25, 70];
 
 // Categorize each age using if-else statements
 ages.forEach(age => {
-  let category = '';
+  let category = ''
+
+  if (age < 13) {
+
+    category = `child`
+
+  } else if (age < 20) {
+
+    category = `teen`
+
+  } else if (age < 65) {
+
+    category = `adult`
+
+  } else if (age > 65) {
+
+    category = `senior`
+
+  }
+
 
   // Write your if-else statements here using specified age ranges
 
